539040a6c810ad114c1a71f48e62e1e270119779
Lobi Blümelein <leano.matschbeere@gmail.com>
2026-03-25 13:56:23 +0100

    Update SERVER_URL to production environment

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Tausche diese Adresse gegen deine echte Server-URL aus, wenn du ihn hochgeladen hast!
-const SERVER_URL = 'nanosliter-github-io.railway.internal'; // Beispiel
+const SERVER_URL = 'nanoslitergithubio-production.up.railway.app'; // Beispiel
 
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
